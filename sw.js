be4421e8f4b9fde64de286e7d37a7951d7da7ea3
couleesXD <132744240+couleesXD@users.noreply.github.com>
2026-02-22 05:57:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-const CACHE_NAME = 'planner-v2';
+// sw.js - GitHub Pages совместимый
+const CACHE_NAME = 'day-planner-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  '/icons/icon-512.svg'
+    '/',
+    '/index.html',
+    '/icons/icon-192.png',
+    '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+            .catch(() => console.log('Cache failed - offline mode limited'))
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
